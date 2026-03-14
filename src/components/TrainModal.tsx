f16d5a059abb962540287e47f99ea9a1e6ba5bbf
unknown <kimtaehyeon9@gmail.com>
2026-03-14 10:28:04 +0900

    feat: 팝업(모달) UI 및 기능 개선
    - 모바일 하단 팝업을 중앙 모달로 변경
    
    - 팝업 헤더에 실제 열차 시발역 표시
    
    - 팝업 내 출도착 시간 표시 형식 변경
    
    - 팝업 내 버튼 및 폰트 스타일 조정

--- src/components/TrainModal.tsx
+++ src/components/TrainModal.tsx
@@ -23,7 +23,7 @@ const TrainModal = ({ train, stops, loading, depStation, arrStation, targetDate,
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
         <div className="modal-header">
-          <div className="train-title">{train.type}#{formatTrainNo(train.trainNo)} {depStation}→{train.destination}</div>
+          <div className="train-title">{train.type}#{formatTrainNo(train.trainNo)} {(stops.length > 0 ? stops[0].station : '...')}→{train.destination}</div>
           <div className="route-summary">
             {depStation} <span className="time-small">({train.depTime})</span> 
             <span> → </span> 
@@ -40,22 +40,8 @@ const TrainModal = ({ train, stops, loading, depStation, arrStation, targetDate,
                 <div key={idx} className="station-row">
                   <div className="station-name" style={{ whiteSpace: 'normal', wordBreak: 'keep-all' }}>{stop.station}</div>
                   <div className="station-times">
-                    <div className="time-box">
-                      {idx !== 0 && stop.arrTime !== '--:--' && (
-                        <>
-                          <span className="time-label-small">도착</span>
-                          <span className="time-value">{stop.arrTime}</span>
-                        </>
-                      )}
-                    </div>
-                    <div className="time-box">
-                      {idx !== stops.length - 1 && stop.depTime !== '--:--' && (
-                        <>
-                          <span className="time-label-small">출발</span>
-                          <span className="time-value">{stop.depTime}</span>
-                        </>
-                      )}
-                    </div>
+                    <span className="arr-time">{idx !== 0 && stop.arrTime !== '--:--' ? stop.arrTime : '\u00A0\u00A0\u00A0\u00A0\u00A0'}</span>
+                    <span className="dep-time">{idx !== stops.length - 1 && stop.depTime !== '--:--' ? stop.depTime : '\u00A0\u00A0\u00A0\u00A0\u00A0'}</span>
                   </div>
                 </div>
               ))}
